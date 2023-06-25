a4be0d855e605611a1b02d3c00813244e4de9924
Ivona Josipovic <josipovic.ivona@gmail.com>
2023-06-25 09:38:10 +0200

    File cleanup and routes update

--- mern-exercise-tracker/backend/routes/exercises.js
+++ mern-exercise-tracker/backend/routes/exercises.js
@@ -50,6 +50,6 @@ router.route('/add').post((req, res) => {
       .catch(err => res.status(400).json('Error: ' + err));
   })
    .catch(err => res.status(400).json('Error: ' + err));
- })
+ });
 
 module.exports = router;
