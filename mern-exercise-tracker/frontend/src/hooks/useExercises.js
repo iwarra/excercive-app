67bcbab7c8c4c7cd3aef8b81dedb8d937ebc173f
Ivona Josipovic <josipovic.ivona@gmail.com>
2023-07-17 18:02:14 +0200

    Added frontend folder for separation

--- mern-exercise-tracker/frontend/src/hooks/useExercises.js
+++ mern-exercise-tracker/frontend/src/hooks/useExercises.js
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import SingleExercise from '../components/SingleExercise';
-import useUser from './useUser';
 
 const useExercises = () => {
   const [exercises, setExercises] = useState([]);
