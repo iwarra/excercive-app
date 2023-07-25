ba1b52d42a8e74eb67533cb19a2d2271559f86f4
Ivona Josipovic <josipovic.ivona@gmail.com>
2023-07-25 17:21:56 +0200

    Added ID

--- mern-exercise-tracker/backend/models/exercise.model.js
+++ mern-exercise-tracker/backend/models/exercise.model.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const exerciseSchema = new Schema({
+  userID: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
   username: {type: String, required: true},
   description: {type: String, required: true},
   duration: {type: Number, required: true},
