fa6f299ab3b582804f295446d203f33add2e4b95
Ivona Josipovic <josipovic.ivona@gmail.com>
2023-09-18 16:00:29 +0200

    Added Netlify to Cors config

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,10 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const corsOptions = {
-    origin: "http://localhost:3000" // frontend URI (ReactJS)
+    origin: [
+      "http://localhost:3000",
+      "https://exerciser.netlify.app"
+    ] 
 }
 
 app.use(cors(corsOptions));
