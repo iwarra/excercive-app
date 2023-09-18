8803ccefa1503863adb6a97d9308bb0f6632e162
Ivona Josipovic <josipovic.ivona@gmail.com>
2023-09-18 15:47:01 +0200

    Cors update

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,11 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+const corsOptions = {
+    origin: "http://localhost:3000" // frontend URI (ReactJS)
+}
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
